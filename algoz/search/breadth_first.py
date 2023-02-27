1c8292c889297ed495161068b86e9cc72930621f
Nick Thorpe <nickthorpe71@gmail.com>
2023-02-26 19:10:07 -0600

    add print statements to bfs

--- algoz/search/breadth_first.py
+++ algoz/search/breadth_first.py
@@ -3,12 +3,16 @@ from data_structures.binary_tree import BinaryTree
 
 def search(args):
     bt = BinaryTree()
+
+    print("Creating binary tree from file...")
     bt.create_from_file(args.file)
+    print("Binary tree created")
 
     if not args.order == "level-order":
         print("Sorry, only level order traversal is supported for breadth first search")
         return
 
+    print("Searching tree for word...")
     print("Found word!" if bfs(bt.root, args.word) else "Word not found")
 
 
